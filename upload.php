26ce653ee382c2549a8d6f428a21d49ac83f4881
غلام حسن احسانی <79522677+gholamhasanEhsani@users.noreply.github.com>
2022-12-30 16:32:33 +0330

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,6 +3,4 @@ $file_name = basename($_FILES["image"]["name"]);
 
 $file_temp = $_FILES["image"]["tmp_name"];
 
-move_uploaded_file( $file_temp, $file_name );
-
-echo "success";
+move_uploaded_file( $file_temp, "files".$file_name );
